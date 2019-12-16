c88de45893389ab384036c281e37d1ff0b05e148
Pioner174 <Romashka951@gmail.com>
2019-12-17 00:43:12 +0500

    Работа с базами завершена, выстроены модели. Подключены к админу.

--- antidas/admin.py
+++ antidas/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Registration, table_link
 
-# Register your models here.
+admin.site.register(Registration)
+admin.site.register(table_link)
--- antidas/models.py
+++ antidas/models.py
@@ -1,6 +1,18 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class Registration(models.Model):
-    login = models.CharField(max_length=25)
-    email = models.EmailField(max_length=100)
-    
+    login = models.CharField(max_length=25, help_text=_('Enter login!'))
+    email = models.EmailField(max_length=100, help_text=_('Enter email!'))
+    password = models.CharField(max_length=25, help_text=_('Enter password!'))
+    date_create = models.DateField(auto_now_add=True)
+    def __str__(self):
+        return self.login
+
+class table_link(models.Model):
+    full_link = models.URLField(max_length=500, help_text=_('Enter full link!'))
+    short_link = models.URLField(help_text=_('Enter Short link!'))
+    number_of_clicks = models.IntegerField(help_text=_('Number of clicks on the link'),default=0)
+    id_registration = models.ForeignKey(Registration, on_delete=models.CASCADE, null=True, blank=True)  #Возможно надо поменять on_delete=SET_NULL
+    def __str__(self):
+        return self.short_link
